34c5d70659330419a4a3831f260cf84a1803afa7
Jared Edwards <84925852+nothinginfinity@users.noreply.github.com>
2026-06-08 18:12:07 -0700

    fix: clean v004-clone source, renamed identifiers to v005-dev (replaces corrupted multipart blob)

--- workers/contractor-v005-dev/contractor-v005-dev.js
+++ workers/contractor-v005-dev/contractor-v005-dev.js
@@ -1,10 +1,18 @@
 var __defProp = Object.defineProperty;
 var __name = (target, value) => __defProp(target, "name", { value, configurable: true });
 
-// contractor-v005-dev.js
+// contractor-v005-dev.js (cloned from v004-demo, identifiers renamed)
 var __defProp2 = Object.defineProperty;
 var __name2 = /* @__PURE__ */ __name((target, value) => __defProp2(target, "name", { value, configurable: true }), "__name");
 var __defProp22 = Object.defineProperty;
 var __name22 = /* @__PURE__ */ __name2((target, value) => __defProp22(target, "name", { value, configurable: true }), "__name");
 var VERSION = "0.7.0";
 var WORKER = "contractor-v005-dev";
+var COMPANY = "CCS Services Group";
+var PHONE = "(818) 624-7212";
+var PHONE_URL = "tel:+18186247212";
+var LICENSE = "CSLB #890991";
+var EMBED_MODEL = "@cf/baai/bge-base-en-v1.5";
+var CHAT_MODEL = "@cf/meta/llama-3.1-8b-instruct";
+var R2_PREFIX = "contractor-v003-2/";
+var PLACEHOLDER_IMG = "https://images.unsplash.com/photo-1504307651254-35680f356dfd?w=1200&q=80";
